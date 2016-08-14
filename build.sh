4194fa9a6de6f334dc49ada8d9a8aef86e96c794
Jed Lippold <jlippold@datacation.com>
2016-08-14 18:15:37 -0400

    fixes build process

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 webpack --config webpack.production.js
 
-scp -rpq "/Users/Jed/Home.Web/public" jed.bz:/cygdrive/d/www/jed.bz
-scp -rpq "/Users/Jed/Home.Web/index.html" jed.bz:/cygdrive/d/www/jed.bz/index.html
+scp -rpq "/Users/Jed/Home.Web/pub" jed.bz:/cygdrive/d/www/jed.bz
+scp -rpq "/Users/Jed/Home.Web/index.html" jed.bz:/cygdrive/d/www/jed.bz/admin/index.html
